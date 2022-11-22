2565120a5a593c3943137a7adc25f43fb0305518
GH-James <goodingj@tcd.ie>
2022-11-22 21:08:07 +0000

    Removed current review from knn fit getting knn_prediction values for training linear regression model Also added summary print at end

--- recommender/content_based_recommendation.py
+++ recommender/content_based_recommendation.py
@@ -2,30 +2,32 @@ import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.neighbors import KNeighborsRegressor
+from sklearn.preprocessing import PolynomialFeatures
 import random
 import glob
 
 
 def mean_square_error(estimates, targets):
-    square_error_values = list()
-    for index, target in enumerate(targets):
-        if type(estimates) in [float, np.float64]:
-            square_error_values.append((target - estimates) ** 2)
-        else:
-            square_error_values.append((target - estimates[index]) ** 2)
+    if type(estimates) in [float, np.float64]:
+        square_error_values = [(target - estimates) ** 2 for target in targets]
+    else:
+        square_error_values = [(target - estimate) ** 2 for estimate, target in zip(estimates, targets)]
     return np.mean(square_error_values)
 
 
-def compare_performance(predictions, user_mean, df):
+def compare_performance(predictions, knn_values, user_mean, df):
     mse_model = mean_square_error(predictions, df["user_rating"])
-    print(f"mean square error model: {mse_model}")
+    print(f"MSE model: {mse_model}")
+    mse_knn = mean_square_error(knn_values, df["user_rating"])
+    print(f"MSE knn: {mse_knn}")
     mse_mean_user_rating = mean_square_error(user_mean, df["user_rating"])
-    print(f"mean square error dummy user rating: {mse_mean_user_rating}")
+    print(f"MSE dummy user rating: {mse_mean_user_rating}")
     mse_mean_business_rating = mean_square_error(df["rating"], df["user_rating"])
-    print(f"mean square error dummy business rating: {mse_mean_business_rating}")
+    print(f"MSE dummy business rating: {mse_mean_business_rating}")
+    return mse_model, mse_knn, mse_mean_user_rating, mse_mean_business_rating
 
 
-def get_train_test_df(csv, test_proportion=0.4):
+def get_train_test_df(csv, test_proportion=0.2):
     df = pd.read_csv(csv)
     test = random.sample(range(len(df)), int(len(df)*test_proportion))
     train = list(set(range(len(df))) - set(test))
@@ -53,17 +55,37 @@ def get_bound_predictions(model, features):
     return predictions
 
 
+def get_knn_training_values(features, targets):
+    knn_values = list()
+    for index in range(len(features)):
+        knn_model = get_knn_model(features.drop(index), targets.drop(index))
+        knn_value = knn_model.predict(features.loc[index:index, :])
+        knn_values.append(knn_value)
+    return knn_values
+
+
 def main():
     random.seed(2e8 + 2e12 + 54056869)
     user_csvs = glob.glob("user_csvs/*")
+
+    mse_models_train = list()
+    mse_knns_train = list()
+    mse_user_train = list()
+    mse_business_train = list()
+
+    mse_models_test = list()
+    mse_knns_test = list()
+    mse_user_test = list()
+    mse_business_test = list()
+
     for csv in user_csvs:
-        print(f"\n\n{csv}\n")
+        print(f"\n\n{csv}")
         train_df, test_df = get_train_test_df(csv)
         train_user_mean_rating = train_df["user_rating"].mean()
         train_content_features = train_df.iloc[:, 5::]
         knn_model = get_knn_model(train_content_features, train_df["user_rating"])
 
-        knn_values = knn_model.predict(train_content_features)
+        knn_values = get_knn_training_values(train_content_features, train_df["user_rating"])
         train_df["knn_value"] = knn_values
 
         regression_model = get_regression_model(train_df.loc[:, ["review_count", "rating", "price", "knn_value"]],
@@ -72,16 +94,40 @@ def main():
         predictions = get_bound_predictions(regression_model,
                                             train_df.loc[:, ["review_count", "rating", "price", "knn_value"]])
 
-        print("\n\nTRAINING PERFORMANCE\n")
-        compare_performance(predictions, train_user_mean_rating, train_df)
+        print("\nTRAINING PERFORMANCE\n")
+        mse_model, mse_knn, mse_mean_user_rating, mse_mean_business_rating = compare_performance(predictions,
+                                                                                                 knn_values,
+                                                                                                 train_user_mean_rating,
+                                                                                                 train_df)
+        mse_models_train.append(mse_model)
+        mse_knns_train.append(mse_knn)
+        mse_user_train.append(mse_mean_user_rating)
+        mse_business_train.append(mse_mean_business_rating)
 
         test_content_features = test_df.iloc[:, 5::]
         knn_values = knn_model.predict(test_content_features)
         test_df["knn_value"] = knn_values
         predictions = get_bound_predictions(regression_model,
                                             test_df.loc[:, ["review_count", "rating", "price", "knn_value"]])
-        print("\n\nTEST PERFORMANCE\n")
-        compare_performance(predictions, train_user_mean_rating, test_df)
+        print("\nTEST PERFORMANCE")
+
+        mse_model, mse_knn, mse_mean_user_rating, mse_mean_business_rating = compare_performance(predictions, knn_values, train_user_mean_rating, test_df)
+        mse_models_test.append(mse_model)
+        mse_knns_test.append(mse_knn)
+        mse_user_test.append(mse_mean_user_rating)
+        mse_business_test.append(mse_mean_business_rating)
+
+    print("\nTRAIN AVERAGE")
+    print(f"avg MSE model: {np.mean(mse_models_train)}")
+    print(f"avg MSE knn: {np.mean(mse_knns_train)}")
+    print(f"avg MSE user: {np.mean(mse_user_train)}")
+    print(f"avg MSE business: {np.mean(mse_business_train)}")
+
+    print("\nTEST AVERAGE")
+    print(f"avg MSE model: {np.mean(mse_models_test)}")
+    print(f"avg MSE knn: {np.mean(mse_knns_test)}")
+    print(f"avg MSE user: {np.mean(mse_user_test)}")
+    print(f"avg MSE business: {np.mean(mse_business_test)}")
 
 
 if __name__ == "__main__":
