0616d059b5bcfb913f4b5dd64c760ff7b5bd84de
AWebb-git <79225323+AWebb-git@users.noreply.github.com>
2022-12-01 15:10:48 +0000

    Added micro metrics to SVM

--- svm/svm_classifier.py
+++ svm/svm_classifier.py
@@ -17,6 +17,24 @@ def get_data(filename):
         X = data.loc[:, "review_count":"price"]
     return X, Y
 
+def get_micro_metrics(confus_mat):
+    tp = []
+    fp = []
+    fn = []
+    tn = []
+    ind = [0, 1, 2, 3, 4]
+    for i in ind:
+        ind_i = ind.copy()
+        ind_i.remove(i)
+        tp.append(confus_mat[i, i])
+        fp.append(sum(confus_mat[ind_i, i]))
+        fn.append(sum(confus_mat[i, ind_i]))
+        tn.append(sum(confus_mat[ind_i, ind_i]))
+    micro_acc = (sum(tp) + sum(tn))/(sum(tp) + sum(tn) + sum(fp) + sum(fn))
+    micro_prec = sum(tp)/(sum(tp) + sum(fp))
+    micro_tpr = sum(tp)/(sum(tp) + sum(fn))
+    micro_fpr = sum(fp)/(sum(fp) + sum(tn))
+    return micro_acc, micro_prec, micro_tpr, micro_fpr
 
 if __name__ == "__main__":
     X, Y = get_data("formatted_data.csv")
@@ -39,12 +57,20 @@ if __name__ == "__main__":
         # scores.append([acc, c, kernel, deg])
         kf = KFold(n_splits=5, shuffle=True, random_state=5)
         scores = []
+        first = True
         for train, test in kf.split(X):
             svm_model.fit(X.loc[train], Y[train])
             preds = svm_model.predict(X.loc[test])
             scores.append(f1_score(Y[test], preds, average="micro"))
-            print(confusion_matrix(Y[train], svm_model.predict(X.loc[train])))
-            print(confusion_matrix(Y[test], preds))
+            #print(confusion_matrix(Y[train], svm_model.predict(X.loc[train])))
+            #print(confusion_matrix(Y[test], preds))
+            if first:
+                first = False
+                micro_con_mat = confusion_matrix(Y[test], preds)
+            else:
+                micro_con_mat = np.add(micro_con_mat, confusion_matrix(Y[test], preds))
+        print(micro_con_mat)
+        get_micro_metrics(micro_con_mat)
         mean_error.append(np.array(scores).mean())
         std_error.append(np.array(scores).std())
 plt.figure(num=3)
